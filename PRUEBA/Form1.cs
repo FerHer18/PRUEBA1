148d33db0104dcc0467d4db8fd5c0a0aab6eb823
Vanessa Carolina Torres Rojas <torres100vane124@gmail.com>
2024-12-03 11:08:00 -0600

    este quiere

--- PRUEBA/Form1.cs
+++ PRUEBA/Form1.cs
@@ -6,5 +6,13 @@ namespace PRUEBA
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            this.Hide();
+            f2.ShowDialog();
+            this.Show();
+        }
     }
 }
